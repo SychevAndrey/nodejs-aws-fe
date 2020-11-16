fc7acbb482f204a809189b230aa9dd47c1ce0aca
Andrey S <nukemoore@gmail.com>
2020-11-16 09:53:06 +0300

    update vars

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product:
     'https://t99kw1qz5d.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://t99kw1qz5d.execute-api.eu-west-1.amazonaws.com/dev/import',
+  import: 'https://7cp8gx70dh.execute-api.eu-west-1.amazonaws.com/dev/import',
   bff: 'https://t99kw1qz5d.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
