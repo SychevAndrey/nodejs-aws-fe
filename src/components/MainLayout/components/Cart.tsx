5d11401b8e539dfde45cb0068b2e37df8b232e21
Andrey S <nukemoore@gmail.com>
2020-12-07 11:56:03 +0300

    update files and api path

--- src/components/MainLayout/components/Cart.tsx
+++ src/components/MainLayout/components/Cart.tsx
@@ -1,24 +1,46 @@
-import Badge from "@material-ui/core/Badge";
-import CartIcon from "@material-ui/icons/ShoppingCart";
-import IconButton from "@material-ui/core/IconButton";
-import React from "react";
-import { useSelector } from "react-redux";
-import { selectCartItems } from "store/cartSlice";
+import Badge from '@material-ui/core/Badge';
+import CartIcon from '@material-ui/icons/ShoppingCart';
+import IconButton from '@material-ui/core/IconButton';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCartItems, updateFromApi } from 'store/cartSlice';
 import { Link } from 'react-router-dom';
+import API_PATHS from '../../../constants/apiPaths';
 
 export default function Cart() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const token = Buffer.from(`SychevAndrey:TEST_PASSWORD`).toString('base64');
+    localStorage.setItem('authorization_token', token);
+    axios
+      .get(`${API_PATHS.cart}/profile/cart`, {
+        headers: {
+          Authorization: `Basic ${localStorage.getItem('authorization_token')}`,
+        },
+      })
+      .then(
+        ({
+          data: {
+            data: { cart },
+          },
+        }) => {
+          dispatch(updateFromApi(cart));
+        }
+      );
+  }, []);
   const cartItems = useSelector(selectCartItems);
   const badgeContent = cartItems.length || undefined;
 
   return (
-    <IconButton 
-      aria-label="show 4 new mails" 
-      color="inherit" 
-      component={Link} 
-      to="/cart"
+    <IconButton
+      aria-label='show 4 new mails'
+      color='inherit'
+      component={Link}
+      to='/cart'
     >
-      <Badge badgeContent={badgeContent} color="secondary">
-        <CartIcon/>
+      <Badge badgeContent={badgeContent} color='secondary'>
+        <CartIcon />
       </Badge>
     </IconButton>
   );
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://7cp8gx70dh.execute-api.eu-west-1.amazonaws.com/dev/import',
   bff: 'https://t99kw1qz5d.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://t99kw1qz5d.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://aueiz4vrl0.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
